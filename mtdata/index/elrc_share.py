794c8608bfef6e3816bf4356716760526f7a5116
Kenneth Heafield <github@kheafield.com>
2021-05-29 14:12:36 +0100

    NLOD-1.0 files now available for download
    Remove post column from file

--- mtdata/index/elrc_share.py
+++ mtdata/index/elrc_share.py
@@ -9,9 +9,7 @@ REFS_FILE = Path(__file__).parent / 'elrc_share.tsv'
 def load_all(index: Index):
     with open(REFS_FILE) as data:
         for line in data:
-            l1, l2, num, short, name, info, download, post, licenses, in_paths = line.split('\t', maxsplit=9)
+            l1, l2, num, short, name, info, download, licenses, in_paths = line.split('\t', maxsplit=8)
             in_paths = in_paths.strip().split('\t')
-            # TODO: corpora that require an HTTP POST to download
-            if len(post) == 0:
-                ent = Entry(langs=(l1, l2), url=download, name="ELRC_" + short, filename="ELRC_" + str(num) + ".zip", in_ext='tmx', in_paths=in_paths)
-                index.add_entry(ent)
+            ent = Entry(langs=(l1, l2), url=download, name="ELRC_" + short, filename="ELRC_" + str(num) + ".zip", in_ext='tmx', in_paths=in_paths)
+            index.add_entry(ent)
