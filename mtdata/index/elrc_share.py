475f4780931e6ff94953afefdaf20c4c15a4f329
Thamme Gowda <tgowdan@gmail.com>
2021-10-03 12:07:10 -0700

    Force unicode encoding (Closes #71)

--- mtdata/index/elrc_share.py
+++ mtdata/index/elrc_share.py
@@ -6,10 +6,12 @@ from pathlib import Path
 from mtdata.index import Index, Entry
 REFS_FILE = Path(__file__).parent / 'elrc_share.tsv'
 
+
 def load_all(index: Index):
-    with open(REFS_FILE) as data:
+    with open(REFS_FILE, encoding='utf-8') as data:
         for line in data:
             l1, l2, num, short, name, info, download, licenses, in_paths = line.split('\t', maxsplit=8)
             in_paths = in_paths.strip().split('\t')
-            ent = Entry(langs=(l1, l2), url=download, name="ELRC_" + short, filename="ELRC_" + str(num) + ".zip", in_ext='tmx', in_paths=in_paths)
+            ent = Entry(langs=(l1, l2), url=download, name="ELRC_" + short, filename="ELRC_" + str(num) + ".zip",
+                        in_ext='tmx', in_paths=in_paths)
             index.add_entry(ent)
