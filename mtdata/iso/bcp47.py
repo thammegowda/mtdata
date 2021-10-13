25c43658e95c38948cdf5e0ad410c1dc390a8e22
Thamme Gowda <tgowdan@gmail.com>
2021-10-12 18:18:49 -0700

    restructure ISO 639; Add tests for BCP47

--- mtdata/iso/bcp47.py
+++ mtdata/iso/bcp47.py
@@ -12,8 +12,9 @@
 from pathlib import Path
 import json
 from mtdata.iso import iso3_code
-
-data_file = Path(__file__).parent / "bcp47.json"
+from dataclasses import dataclass
+from typing import Optional
+from collections import namedtuple
 
 
 def load_json(path: Path):
@@ -22,7 +23,20 @@ def load_json(path: Path):
         return json.load(fp)
 
 
-class BCP47:
+class BCP47Tag(namedtuple('BCP47Tag', ('lang', 'script', 'region', 'tag'))):
+    __slots__ = ()
+
+    def __new__(cls, lang, script: Optional[str]=None, region: Optional[str]=None):
+        tag = ''.join([lang, f'-{script}' if script else '', f'-{region}' if region else ''])
+        obj = super(BCP47Tag, cls).__new__(cls, lang, script, region, tag)
+        #obj.tag = tag
+        return obj
+
+    def __str__(self):
+        return self.tag
+
+
+class BCP47Parser:
 
     def __init__(self, data):
         self.data = data
@@ -30,55 +44,83 @@ class BCP47:
         self.scripts = {code: name for code, name in data['scripts']}
         self.countries = {code: name for code, name in data['countries']}
         self.languages = {code3: (code2, name) for code3, code2, name in data['languages']}
-        for key in self.languages:       # validation
+        for key in self.languages:  # validation
             assert key == iso3_code(key, fail_error=True)
-        self.default_scripts = {}   # these needs suppression; eng-Latn is just eng, as Latn is default
+        self.default_scripts = {}  # these needs suppression; eng-Latn is just eng, as Latn is default
         for lang_code, script_code, lang_name in data['default_scripts']:
             code3 = iso3_code(lang_code, fail_error=True)
             assert script_code in self.scripts
             self.default_scripts[code3] = script_code
 
-    def parse(self, tag):
+    def parse(self, tag) -> BCP47Tag:
+        """
+        Parameters
+        ----------
+        tag : tag to be parsed
+
+        Returns
+        -------
+            BCP47Tag
+        """
         code_orig = tag
         tag = tag.replace('_', '-').strip()
         assert tag
         parts = tag.split('-')
         assert 1 <= len(parts) <= 3, f'BCP47 code longer than 3 parts not supported yet; given {code_orig}'
-        lang, script, country = None, None, None
+        lang, script, region = None, None, None
         # part 1: it has to be language
         lang = iso3_code(parts[0], default=None)
         assert lang, f'Unable to recognize {code_orig}; Unknown language'
         assert lang in self.languages, f'{lang} is invalid'
 
         parts = parts[1:]
-        if parts:  # part 2 can be either script or country code
+        if parts:  # part 2 can be either script or region code
             if parts[0].title() in self.scripts:
                 script = parts[0].title()
             elif parts[0].upper() in self.countries:
-                country = parts[0].upper()
+                region = parts[0].upper()
             else:
                 raise ValueError(f'Unable to parse {code_orig}')
             parts = parts[1:]
-        if parts:   # part 3, if it exists, must be a country
+        if parts:  # part 3, if it exists, must be a region
             assert script
-            assert not country
+            assert not region
             if parts[0].upper() in self.countries:
-                country = parts[0].upper()
+                region = parts[0].upper()
             else:
-                raise ValueError(f"Cant find {code_orig}; Unknown country")
+                raise ValueError(f"Cant find {code_orig}; Unknown region")
             parts = parts[1:]
         assert not parts  # all parts are consumed
         if script and self.default_scripts[lang] == script:
-            script = None   # suppress script
-        return lang, script, country
+            script = None  # suppress script
+        tag = BCP47Tag(lang=lang, script=script, region=region)
+        return tag
 
-    def __call__(self, tag):
+    def __call__(self, tag) -> BCP47Tag:
+        """
+        Parameters
+        ----------
+        tag : tag to be parsed
+        Returns
+        -------
+           BCP47Tag
+        """
         return self.parse(tag)
 
 
-bcp47 = BCP47(data=load_json(data_file))
-
+data_file = Path(__file__).parent / "bcp47.json"
+bcp47_data = load_json(data_file)
+bcp47 = BCP47Parser(data=bcp47_data)
 
 if __name__ == '__main__':
-    for i in ["en-GB", "en", "en-Latn-US", "en-IN", "en-Latn-GB"]:
-        print(i, bcp47.parse(i))
+    import argparse
+
+    p = argparse.ArgumentParser(prog='python -m mtdata.iso.bcp47', description="BCP47 lookup tool")
+    p.add_argument("langs", nargs='+', help="Language code or name that needs to be looked up. "
+                                            "When no language code is given, all languages are listed.")
+    args = vars(p.parse_args())
+    if args.get('langs'):
+        print("INPUT\tSTD\tLANG\tSCRIPT\tREGION")
+        for inp in args['langs']:
+            tag = bcp47(inp)
+            print(f'{inp}\t{tag}\t{tag.lang}\t{tag.script}\t{tag.region}')
--- tests/test_bcp47.py
+++ tests/test_bcp47.py
@@ -3,20 +3,20 @@
 #
 # Author: Thamme Gowda
 # Created: 10/4/21
-from mtdata.iso.bcp47 import bcp47
+from mtdata.iso.bcp47 import bcp47, BCP47Tag
 from pytest import fail
 
 
 def test_bcp47():
-    assert bcp47("en-GB") == ('eng', None, 'GB')
-    assert bcp47("en") == ('eng', None, None)
-    assert bcp47("en-IN") == ('eng', None, 'IN')
-    assert bcp47("en-US") == ('eng', None, 'US')
+    assert bcp47("en-GB") == ('eng', None, 'GB', 'eng-GB')
+    assert bcp47("en") == ('eng', None, None, 'eng')
+    assert bcp47("en-IN") == ('eng', None, 'IN', 'eng-IN')
+    assert bcp47("en-US") == ('eng', None, 'US', 'eng-US')
     # Latn script is default, so None
-    assert bcp47("en-Latn") == ('eng', None, None)
+    assert bcp47("en-Latn") == ('eng', None, None, 'eng')
     # hypothetical;
-    assert bcp47("en-Knda") == ('eng', 'Knda', None)
-    assert bcp47("en-Latn-GB") == ('eng', None, 'GB')
+    assert bcp47("en-Knda") == ('eng', 'Knda', None, 'eng-Knda')
+    assert bcp47("en-Latn-GB") == ('eng', None, 'GB', 'eng-GB')
 
     try:
         bcp47("en-Latn-UK")
@@ -24,18 +24,33 @@ def test_bcp47():
     except ValueError:
         pass  # expected
 
-    assert bcp47("kn") == ('kan', None, None)
-    assert bcp47("kn-Knda") == ('kan', None, None)    # default script
-    assert bcp47("kn-Knda-IN") == ('kan', None, 'IN')
-    assert bcp47("kn_Knda_IN") == ('kan', None, 'IN')
-    assert bcp47("kn_Knda_in") == ('kan', None, 'IN')
-    assert bcp47("kn_Latn_IN") == ('kan', 'Latn', 'IN')
-    assert bcp47("kn_Deva_IN") == ('kan', 'Deva', 'IN')
-    assert bcp47("hi_Deva_IN") == ('hin', None, 'IN')    # default script
+    assert bcp47("kn") == ('kan', None, None, 'kan')
+    assert bcp47("kn-Knda") == ('kan', None, None, 'kan')    # default script
+    assert bcp47("kn-Knda-IN") == ('kan', None, 'IN', 'kan-IN')
+    assert bcp47("kn_Knda_IN") == ('kan', None, 'IN', 'kan-IN')
+    assert bcp47("kn_Knda_in") == ('kan', None, 'IN', 'kan-IN')
+    assert bcp47("kn_Latn_IN") == ('kan', 'Latn', 'IN', 'kan-Latn-IN')
+    assert bcp47("kn_Deva_IN") == ('kan', 'Deva', 'IN', 'kan-Deva-IN')
+    assert bcp47("hi_Deva_IN") == ('hin', None, 'IN', 'hin-IN')    # default script
 
-    assert bcp47("pt_PT") == ('por', None, 'PT')
-    assert bcp47("pt_pt") == ('por', None, 'PT')
-    assert bcp47("pt_BR") == ('por', None, 'BR')
-    assert bcp47("pt_br") == ('por', None, 'BR')
-    assert bcp47("fr") == ('fra', None, None)
-    assert bcp47("fr-CA") == ('fra', None, 'CA')
+    assert bcp47("pt_PT") == ('por', None, 'PT', 'por-PT')
+    assert bcp47("pt_pt") == ('por', None, 'PT', 'por-PT')
+    assert bcp47("pt_BR") == ('por', None, 'BR', 'por-BR')
+    assert bcp47("pt_br") == ('por', None, 'BR', 'por-BR')
+    assert bcp47("pt_Latn_br") == ('por', None, 'BR', 'por-BR')  # default script
+    assert bcp47("pt_Cyrl_br") == ('por', 'Cyrl', 'BR', 'por-Cyrl-BR')  # non default script
+    assert bcp47("fr") == ('fra', None, None, 'fra')
+    assert bcp47("fr-CA") == ('fra', None, 'CA', 'fra-CA')
+
+
+def test_py_obj_model():
+    """Test cases for python object model"""
+    assert bcp47("en-GB") is not bcp47("en-GB")  # object comparison: two objects are two different references
+    assert bcp47("en-GB") == bcp47("en-GB")      # but they have same value and hence equal
+    assert bcp47("en-GB") == bcp47("en-Latn-GB")  # ignore the default script
+    assert bcp47("en-Latn-US") == bcp47("en-US")  # ignore default script again
+    assert bcp47("en-Latn") == bcp47("en")  # ignore default script again
+    assert bcp47("en-Latn") == bcp47("english")  # ignore default script again
+    assert bcp47("en-Latn") == bcp47("English")  # ignore default script again
+    assert bcp47("en-Latn") == bcp47("ENG")  # ignore default script again
+    assert bcp47("en-US") != bcp47("en")      # dont ignore region
