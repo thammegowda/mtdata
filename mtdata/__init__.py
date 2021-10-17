92bd398d3d4204e704913d34466e6c5a7d6d3e35
Thamme Gowda <tgowdan@gmail.com>
2021-10-16 21:28:12 -0700

    Update CHANGELOG, englighten package import failure is now skipped (for local setup.py)

--- mtdata/__init__.py
+++ mtdata/__init__.py
@@ -10,12 +10,17 @@ __author__ = 'Thamme Gowda'
 import logging as log
 from pathlib import Path
 import os
-import enlighten
 
-pbar_man = enlighten.get_manager()
 
 debug_mode = False
 _log_format = '%(asctime)s %(module)s.%(funcName)s:%(lineno)s %(levelname)s:: %(message)s'
 log.basicConfig(level=log.INFO, datefmt='%Y-%m-%d %H:%M:%S', format=_log_format)
 cache_dir = Path(os.environ.get('MTDATA', '~/.mtdata')).expanduser()
 cached_index_file = cache_dir / f'mtdata.index.{__version__}.pkl'
+
+try:
+    import enlighten
+    pbar_man = enlighten.get_manager()
+except ImportError as e:
+    log.warning("enlighten package maybe required. please run 'pip install englighten'")
+    log.warning(e)
