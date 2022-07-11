1fbd37a9435a8ec73a5a0d789bb2602974638407
Thamme Gowda <tgowdan@gmail.com>
2022-07-11 12:49:25 -0700

    version 0.3.7

--- mtdata/__init__.py
+++ mtdata/__init__.py
@@ -4,7 +4,7 @@
 # Created: 4/4/20
 
 
-__version__ = '0.3.7-dev'
+__version__ = '0.3.7'
 __description__ = 'mtdata is a tool to download datasets for machine translation'
 __author__ = 'Thamme Gowda'
 
