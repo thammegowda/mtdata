a50b817b8c9105e282620731e4b1e6b1c6f4f134
Thamme Gowda <tgowdan@gmail.com>
2023-03-19 17:31:55 -0700

    nllb lang pairs simplification

--- mtdata/index/allenai_nllb.py
+++ mtdata/index/allenai_nllb.py
@@ -11,12 +11,12 @@ _ALLENAI_URL = "https://storage.googleapis.com/allennlp-data-bucket/nllb/"
 _LICENSE = "https://opendatacommons.org/licenses/by/1-0/"
 
 def load_all(index: Index):
-
     nllb_pairs = json.load(open(resource_dir / "allenai_nllb.json"))
     # these bib keys are as per listed on https://huggingface.co/datasets/allenai/nllb 
     bibkeys = ('schwenk-etal-2021-ccmatrix', 'heffernan2022bitext', 'nllb-2022')
 
-    for src_lang, tgt_lang in nllb_pairs:
+    for pair in nllb_pairs:
+        src_lang, tgt_lang = pair.split('-')
         download_url = f"{_ALLENAI_URL}{src_lang}-{tgt_lang}.gz"
         ent = Entry(did=DatasetId(group='AllenAi', name="nllb", version='1', langs=(src_lang, tgt_lang)),
             cite=bibkeys, ext="tsv.gz", url=download_url)
