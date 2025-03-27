5cf8fbe5ab8e9a15c9a5fdfb3f2cd0075293a1b0
Thamme Gowda <thammegowda@microsoft.com>
2025-03-26 19:06:20 -0700

    Fix:  NTREX paths inside tarball: _ -> -; improve error message

--- mtdata/utils.py
+++ mtdata/utils.py
@@ -181,7 +181,7 @@ class TarPath(ArchivedPath):
         self.ext_dir = self.extract()
         matches = list(self.ext_dir.glob(self.name))
         if len(matches) != 1:
-            raise Exception(f'expected to find exactly one path inside tarball, but found {matches}')
+            raise Exception(f'expected to find exactly one path inside tarball @ {self.ext_dir}/{self.name}, but found {matches}')
         self.child = matches[0]
         self.open = self.child.open
 
